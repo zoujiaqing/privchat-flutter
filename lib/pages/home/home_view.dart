30bfa09fee99f2f54c325a13a0b9dd2b5f153913
zoujiaqing <zoujiaqing@gmail.com>
2024-05-04 04:30:43 +0800

    Fix BottomNavigationBar padding for iOS platform.

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -15,6 +17,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double bottomPadding = 0;
+    if (Platform.isIOS) {
+      bottomPadding = 16.h;
+    }
     return Obx(() => Scaffold(
           backgroundColor: Styles.c_FFFFFF,
           body: IndexedStack(
@@ -25,37 +31,40 @@ class HomePage extends StatelessWidget {
               MinePage(),
             ],
           ),
-          bottomNavigationBar: BottomBar(
-            index: logic.index.value,
-            items: [
-              BottomBarItem(
-                selectedImgRes: ImageRes.homeTab1Sel,
-                unselectedImgRes: ImageRes.homeTab1Nor,
-                label: StrRes.home,
-                imgWidth: 28.w,
-                imgHeight: 28.h,
-                onClick: logic.switchTab,
-                onDoubleClick: logic.scrollToUnreadMessage,
-                count: logic.unreadMsgCount.value,
-              ),
-              BottomBarItem(
-                selectedImgRes: ImageRes.homeTab2Sel,
-                unselectedImgRes: ImageRes.homeTab2Nor,
-                label: StrRes.contacts,
-                imgWidth: 28.w,
-                imgHeight: 28.h,
-                onClick: logic.switchTab,
-                count: logic.unhandledCount.value,
-              ),
-              BottomBarItem(
-                selectedImgRes: ImageRes.homeTab4Sel,
-                unselectedImgRes: ImageRes.homeTab4Nor,
-                label: StrRes.mine,
-                imgWidth: 28.w,
-                imgHeight: 28.h,
-                onClick: logic.switchTab,
-              ),
-            ],
+          bottomNavigationBar: Container(
+            padding: EdgeInsets.only(bottom: bottomPadding),
+            child: BottomBar(
+              index: logic.index.value,
+              items: [
+                BottomBarItem(
+                  selectedImgRes: ImageRes.homeTab1Sel,
+                  unselectedImgRes: ImageRes.homeTab1Nor,
+                  label: StrRes.home,
+                  imgWidth: 28.w,
+                  imgHeight: 28.h,
+                  onClick: logic.switchTab,
+                  onDoubleClick: logic.scrollToUnreadMessage,
+                  count: logic.unreadMsgCount.value,
+                ),
+                BottomBarItem(
+                  selectedImgRes: ImageRes.homeTab2Sel,
+                  unselectedImgRes: ImageRes.homeTab2Nor,
+                  label: StrRes.contacts,
+                  imgWidth: 28.w,
+                  imgHeight: 28.h,
+                  onClick: logic.switchTab,
+                  count: logic.unhandledCount.value,
+                ),
+                BottomBarItem(
+                  selectedImgRes: ImageRes.homeTab4Sel,
+                  unselectedImgRes: ImageRes.homeTab4Nor,
+                  label: StrRes.mine,
+                  imgWidth: 28.w,
+                  imgHeight: 28.h,
+                  onClick: logic.switchTab,
+                ),
+              ],
+            ),
           ),
         ));
   }
