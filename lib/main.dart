6f0069afada9bd0a85f959c85f22601d6ac73aab
zoujiaqing <zoujiaqing@gmail.com>
2024-05-20 01:36:04 +0800

    Set touch bar background to empty on Android platform.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:privchat_common/privchat_common.dart';
 
 import 'app.dart';
 
-void main() => Config.init(() => runApp(const ChatApp()));
+void main() {
+  Config.init(() => runApp(const ChatApp()));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
+}
